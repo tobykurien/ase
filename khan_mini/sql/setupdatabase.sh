99f71e33495451405fc1003fde0c2a0009a12cd1
skullkey <schalk.heunis@gmail.com>
2012-06-17 23:02:52 +0200

    simplified data model and sample data'

--- khan_mini/sql/setupdatabase.sh
+++ khan_mini/sql/setupdatabase.sh
@@ -1,2 +1,3 @@
 rm ../db/englishessay.db
 sqlite3 ../db/englishessay.db < setupdatabase.sql   
+sqlite3 ../db/englishessay.db < loadsampledata.sql   
--- khan_mini/sql/setupdatabase.sql
+++ khan_mini/sql/setupdatabase.sql
@@ -1,53 +1,31 @@
 PRAGMA foreign_keys = ON;
 
-drop table if exists student;
-create table student (
- id INTEGER PRIMARY KEY AUTOINCREMENT,
- student_name text);
-
-drop table if exists quiz;
-create table quiz (
- id INTEGER PRIMARY KEY AUTOINCREMENT,
+drop table if exists assignment;
+create table assignment (
+ id INTEGER PRIMARY KEY,
  title text,
  description text,
  state text,
- date text
+ startdatetime text
 );
 
-drop table if exists question;
-create table question (
- id INTEGER PRIMARY KEY AUTOINCREMENT,
- quiz_id INTEGER REFERENCES quiz(id) ON DELETE CASCADE,
- questiontext text);
-
-
-drop table if exists assignment;
-create table assignment(
- id INTEGER PRIMARY KEY AUTOINCREMENT,
- quiz_id INTEGER REFERENCES quiz(id) ON DELETE CASCADE,
- student_id INTEGER REFERENCES student(id) ON DELETE CASCADE,
+drop table if exists essay;
+create table essay(
+ id INTEGER PRIMARY KEY,
+ assignment_id INTEGER REFERENCES assignment(id) ON DELETE CASCADE,
+ student_name text,
+ essay_text text, 
+ submitteddatetime text,
  score real);
 
-drop table if exists answer;
-create table answer(
+drop table if exists essay_eval;
+create table essay_eval(
  id INTEGER PRIMARY KEY AUTOINCREMENT,
  assignment_id INTEGER REFERENCES assignment(id) ON DELETE CASCADE,
- question_id INTEGER REFERENCES question(id) ON DELETE CASCADE,
- answer_text text);
-
-drop table if exists eval;
-create table eval(
- id INTEGER PRIMARY KEY AUTOINCREMENT,
- quiz_id INTEGER REFERENCES quiz(id) ON DELETE CASCADE,
- question_id INTEGER REFERENCES question(id) ON DELETE CASCADE);
-
-drop table if exists answerscore;
-create table answerscore(
- id INTEGER PRIMARY KEY AUTOINCREMENT,
- eval_id INTEGER REFERENCES eval(id) ON DELETE CASCADE, 
- answer_id INTEGER REFERENCES answer(id) ON DELETE CASCADE,
- student_id INTEGER REFERENCES student(id) ON DELETE CASCADE,
- score real);
-
-
-
+ essay1_name text,
+ essay2_name text,
+ essay3_name text,
+ score1 real,
+ score2 real,
+ score3 real 
+);
