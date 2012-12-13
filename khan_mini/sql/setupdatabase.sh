05ff20b6a624eb178067b84ff4625826c304fe19
skullkey <schalk.heunis@gmail.com>
2012-12-13 10:29:26 +0200

    Setting up mysql database user

--- khan_mini/sql/setupdatabase.sh
+++ khan_mini/sql/setupdatabase.sh
@@ -2,6 +2,7 @@
 #--rm ../db/englishessay.db
 #sqlite3 ../db/englishessay.db < setupdatabase.sql   
 #sqlite3 ../db/englishessay.db < loadsampledata.sql   
-
-mysql -u veesda -p veesda < setupdatabase.sql   
-mysql -u veesda -p veesda < loadsampledata.sql   
+echo "Creating the database on mysql using root - enter the password for the root user on mysql"
+mysql -u root -p < setupdatabase.sql   
+echo "Populating with test data - enter the password for the 'ase' user - should be asep4s5"
+mysql -u ase -p ase < loadsampledata.sql   
