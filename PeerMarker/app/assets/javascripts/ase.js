bf5164e88917aa0d12a5035587646542fb36b7ec
skullkey <schalk.heunis@gmail.com>
2013-03-30 15:54:37 +0200

    initial commit

---
+++ PeerMarker/app/assets/javascripts/ase.js
@@ -0,0 +1,15 @@
+function countdown(element, minutes, seconds) {
+    var time = minutes*60 + seconds;
+    var el = document.getElementById(element);
+        if(time == 0) {
+            el.innerHTML = "Please save your work, the assignment time has expired";    
+            clearInterval(interval);
+            return;
+        }
+        var minutes = Math.floor( time / 60 );
+        if (minutes < 10) minutes = "0" + minutes;
+        var seconds = time % 60;
+        if (seconds < 10) seconds = "0" + seconds;
+        var text = minutes + ':' + seconds;
+		return text;
+    }
