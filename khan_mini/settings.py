7ff10ff8b8de1e4189890ff1b376f5a2cb371c15
Live session user <ubuntu@ubuntu.(none)>
2013-04-06 09:32:34 +0000

    tweaks - changed hostnames back to ip address

--- khan_mini/settings.py
+++ khan_mini/settings.py
@@ -1,9 +1,9 @@
 import os
 
-KHAN_BASE_URL = "http://ase:8080"       # instance used by khan mini to access API
-KHAN_MINI_BASE_URL = "http://ase:8081"
-KHAN_BASE_URL2 = "http://ase"           # excluding port, which will be randomly assigned from below
-KHAN_INSTANCES = [8082,8083,8084,8085,8086,8087,8088,8089,8090]
+KHAN_BASE_URL = "http://192.168.1.2:8080"       # instance used by khan mini to access API
+KHAN_MINI_BASE_URL = "http://192.168.1.2:8081"
+KHAN_BASE_URL2 = "http://192.168.1.2"           # excluding port, which will be randomly assigned from below
+KHAN_INSTANCES = [8082,8083,8084,8085,8086]
 
 khanconf = os.path.join(os.path.dirname(__file__), 'khanmini.conf')
 enlishessayconf = os.path.join(os.path.dirname(__file__), 'englishessay.conf')
--- scripts/server/backup.sh
+++ scripts/server/backup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-HOME="/home/ubuntu/"
-DIR=$HOME"moodle/backups/$(date +%a)"
+HOME="/home/ubuntu/moodle/backups"
+DIR=$HOME"/$(date +%a)"
 echo $DIR
 
 mkdir $DIR
@@ -8,16 +8,20 @@ cd $DIR
 #echo Backing up www...
 #zip -r www.zip /var/www/moodle/* 2>/dev/null >/dev/null
 
-echo Backing up database...
+echo Backing up moodle database...
 mysqldump -u moodleuser -pmoodlepass moodle > moodle.sql
 zip mysql.zip  moodle.sql
 rm moodle.sql
 
-echo Backing up data...
-zip -r moodledata.zip ~/moodle/moodledata/* 2>/dev/null >/dev/null
+echo Backing up khan academy database...
+cp /home/ubuntu/khan_academy/code/datastore /home/ubuntu/khan_academy/code/datastore.backup
+zip ka_datastore.zip /home/ubuntu/khan_academy/code/datastore.backup
+
+#echo Backing up data...
+#zip -r moodledata.zip /home/ubuntu/moodle/moodledata/* >/dev/null
 
 echo Uploading backups...
-rsync -ar $DIR ubuntu@cloud.house4hack.co.za:~/moodle/ase_backups
+rsync -ar $HOME ubuntu@cloud.house4hack.co.za:~/moodle/ase_backups
 
 echo Done.
 
