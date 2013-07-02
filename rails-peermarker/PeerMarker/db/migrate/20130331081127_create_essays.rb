ad0afaa289e03aa651bea87bdd0b240869dc9907
tobykurien <tobykurien@gmail.com>
2013-07-02 09:33:45 +0200

    fixed schema for decimal columns

--- rails-peermarker/PeerMarker/db/migrate/20130331081127_create_essays.rb
+++ rails-peermarker/PeerMarker/db/migrate/20130331081127_create_essays.rb
@@ -3,8 +3,8 @@ class CreateEssays < ActiveRecord::Migration
     create_table :essays do |t|
       t.string :studentname
       t.string :essay_text
-      t.decimal :score
-      t.decimal :grade
+      t.decimal :score, :precision => 5, :scale => 2, :default => 0.00
+      t.decimal :grade, :precision => 5, :scale => 2, :default => 0.00
       t.references :assignment
 
       t.timestamps
--- rails-peermarker/PeerMarker/db/migrate/20130402223001_create_essay_evals.rb
+++ rails-peermarker/PeerMarker/db/migrate/20130402223001_create_essay_evals.rb
@@ -5,8 +5,8 @@ class CreateEssayEvals < ActiveRecord::Migration
       t.integer  :assignment_id
       t.integer  :essay1_id
       t.integer  :essay2_id
-      t.decimal  :score1
-      t.decimal  :score2
+      t.decimal  :score1, :precision => 5, :scale => 2, :default => 0.00
+      t.decimal  :score2, :precision => 5, :scale => 2, :default => 0.00
       t.string   :pcomment1
       t.string   :ccomment1
       t.string   :pcomment2
