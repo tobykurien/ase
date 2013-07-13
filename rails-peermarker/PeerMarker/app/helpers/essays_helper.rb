70f27c3d567494070c3cf701858c3c8addcae0a1
skullkey <schalk.heunis@gmail.com>
2013-07-13 17:30:56 +0200

    text nil handled in essay_helper

--- rails-peermarker/PeerMarker/app/helpers/essays_helper.rb
+++ rails-peermarker/PeerMarker/app/helpers/essays_helper.rb
@@ -1,6 +1,7 @@
 module EssaysHelper
   def make_short(text)
-     return text[0,10] + " ..."
+     return text[0,10] + " ..." unless text.nil?
+     return "" if text.nil?
   end
   
   def not_marked(mark)
